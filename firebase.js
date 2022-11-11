1c8b9b30dea00899b03da771ee0e3b477e2a5fff
Bryan Sarmiento <SarmientoB2@gmail.com>
2022-11-11 03:23:14 -0800

    Added Authentication with NextAuth.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB4bpReRqT4Ie8oXoURcQiGDN0PkUhnWY0",
+    authDomain: "react--clone-fffe0.firebaseapp.com",
+    projectId: "react--clone-fffe0",
+    storageBucket: "react--clone-fffe0.appspot.com",
+    messagingSenderId: "1078443099125",
+    appId: "1:1078443099125:web:e6a0f2593fefbe1265d6bc"
+  };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
 import { MenuIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 function Header() {
+    const {data: session} = useSession();
+
   return (
     <header>
         <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
@@ -21,8 +24,10 @@ function Header() {
                 <SearchIcon className='h-12 p-4' />
             </div>
             <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
-                <div className='link'>
-                    <p>Hello Bryan</p>
+                <div onClick={!session ? signIn : signOut} className='link'>
+                    <p>
+                        {session ? `Hello, ${session.user.name}` : 'Sign In'}
+                    </p>
                     <p className='font-extrabold md:text-sm'>Account & Lists</p>
                 </div>
                 <div className='link'>
